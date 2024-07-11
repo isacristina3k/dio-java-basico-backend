6ab78dd653dd3e238459d57f642b58a37e1fd9b5
isacristina3k <isadoranovaes1@gmail.com>
2024-07-11 02:35:40 -0300

    java collections

---
+++ Biblioteca/src/Livro.java
@@ -0,0 +1,41 @@
+
+public class Livro {
+	private String titulo;
+	private String autor;
+	private int anoPublicacao;
+	
+	//construtor com todos os atributos
+	public Livro(String titulo, String autor, int anoPublicacao) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.anoPublicacao = anoPublicacao;
+	}
+	
+	//getters e setters de todos os atributos (atalho alt shift s)
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getAnoPublicacao() {
+		return anoPublicacao;
+	}
+	public void setAnoPublicacao(int anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+
+	@Override
+	public String toString() {
+		return "Livros [titulo=" + titulo + ", autor=" + autor + ", anoPublicacao=" + anoPublicacao + "]";
+	}
+	
+	
+	
+}
